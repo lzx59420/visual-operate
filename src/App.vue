da1e3196f8f963ad4bc40e98d5ca6b5cf614f774
nice-m-y <96524580+nice-m-y@users.noreply.github.com>
2022-09-17 15:41:01 +0800

    init: create program

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did i!" />
+      <a-button
+          shape="circle" size="big"
+          :style="{marginLeft: '10px'}"
+        >2222</a-button>
+        <div ref="target">
+    1111 world
+  </div>
+
+      <nav>
+        <RouterLink to="/">{{c}}</RouterLink>
+        <RouterLink to="/about">{{a}}</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { ref,reactive,computed} from 'vue'
+import { clone } from 'lodash'
+import { onClickOutside } from '@vueuse/core'
+import HelloWorld from './components/HelloWorld.vue'
+
+const a = ref(2)
+const b = reactive([1,2,4])
+
+const target = ref(null)
+onClickOutside(target,e=>console.log(e))
+
+const obj = {a:2} 
+const obj2 = clone(obj)
+console.log(obj2);
+obj2.a = 3 
+console.log(obj,obj2);
+
+const c = computed(()=>a.value * 2)
+
+</script>
+
+<style lang="scss" scoped>
+
+$yellow1:  #FFF701;
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    background-color: $yellow1;
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
